f66eae629e50e8ca449c722a18f99b925bcf56be
Anshu Chaurasia <39023588+chwanshu27@users.noreply.github.com>
2019-03-16 06:43:42 +0530

    Add files via upload

--- solidity/MetroBiki.sol
+++ solidity/MetroBiki.sol
@@ -1,15 +1,22 @@
 pragma solidity ^0.5.0;
 
-contract BicycleRegister {
+import "./ownable.sol";
+
+contract BicycleRegister is Ownable {
 
     event NewBicycle(uint id, string Model, string Color);
 
+    enum State{ NOTFORSALE, FORSALE, HOLD, SOLD } 
+    State state;
     struct Bicycle {
         string Model;
         string Color;
         address Owner;
         uint32 upvoteCount;
         uint32 downvoteCount;
+        uint16 boughtAtTime;
+        uint price;
+        State state;
     }
 
     Bicycle[] public bicycles;
@@ -18,7 +25,7 @@ contract BicycleRegister {
     mapping(address => uint) public ownerBicycleCount;   //
 
     function _registerBicycle(string memory _Model, string memory _Color) private {
-        uint id = bicycles.push(Bicycle(_Model, _Color, msg.sender, 0, 0)) - 1;
+        uint id = bicycles.push(Bicycle(_Model, _Color, msg.sender, 0, 0, 0, 0, State.NOTFORSALE)) - 1; 
         bicycleToOwner[id] = msg.sender;
         ownerBicycleCount[msg.sender]++;
         emit NewBicycle(id, _Model, _Color);
--- solidity/metrobikitrade.sol
+++ solidity/metrobikitrade.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.5.0;
 import "./MetroBiki.sol";
 //conditions under which a reviewer can review...
 
-contract Review is MetroBiki
-{
+contract Review is BicycleRegister {
 
     mapping(address => uint[]) reviewerToBicycle;
 
     modifier notReviewed(uint _bicycleId) {
         uint flag = 0;
-        for(uint i=0; i<reviewerToBicycle[msg.sender].length; i++) {
+        for(uint i = 0; i < reviewerToBicycle[msg.sender].length; i++) {
             if(reviewerToBicycle[msg.sender][i] == _bicycleId) {
                 flag == 1;
                 break;
             }
         }
-        require(flag == 0);
+        require(flag == 0, "Already review submitted!");
         _;
     }
 
     function upvoteBicycle(uint _bicycleId) public notReviewed(uint) {
-        require(msg.sender != bicycleToOwner[_bicycleId]);  
+        require(msg.sender != bicycleToOwner[_bicycleId], "Owner can not review!");  
         Bicycle storage testBicycle = bicycles[_bicycleId];
         testBicycle.upvoteCount++;
     }
